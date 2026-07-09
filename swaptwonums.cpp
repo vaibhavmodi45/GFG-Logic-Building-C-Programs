032db0ea234d28039e4ff584e3e3f856c694c2ad
Vaibhav Modi <123352253+vaibhavmodi45@users.noreply.github.com>
2026-07-09 23:50:38 +0530

    Implement number swapping in swaptwonums.cpp

---
+++ swaptwonums.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cout << "Enter two numbers a & b = ";
+  cin >> a >> b;
+
+  //using third variable
+  int temp = a;
+  a = b;
+  b = temp;
+
+  // without third variable
+  // a = a + b;
+  // b = a - b;
+  // a = a - b;
+
+  cout << "After Swaping" << endl;
+  cout << "a = " << a << " & b = " << b; 
+
+  return 0;
+}
