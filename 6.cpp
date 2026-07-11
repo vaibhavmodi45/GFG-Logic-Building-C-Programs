9cd548c9341ecc51f5571893ed0b52354489119a
Vaibhav Modi <123352253+vaibhavmodi45@users.noreply.github.com>
2026-07-11 23:30:51 +0530

    Add program to display sizes of data types
    Print the sizes of various data types in C++.

---
+++ 6.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 8;
+    float b = 3.14;
+    char c = 'a';
+    double d = 1234546;
+    bool t = true;
+    
+    cout << "size of int is = " << sizeof(a) << endl;
+    cout << "size of float is = " << sizeof(b) << endl;
+    cout << "size of char is = " << sizeof(c) << endl;
+    cout << "size of double is = " << sizeof(d) << endl;
+    cout << "size of bool is = " << sizeof(t) << endl;
+    
+    return 0;
+}
