5574a625bf2e3cf0c305eadae969ef2e357b492e
Vaibhav Modi <123352253+vaibhavmodi45@users.noreply.github.com>
2026-07-15 23:14:44 +0530

    Implement area and perimeter calculation for a rectangle

--- Basic C++ Programs/area-and-perimeter-of-rectangle.cpp
+++ Basic C++ Programs/area-and-perimeter-of-rectangle.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+  int l, b;
+  cout << "Enter length and breadth of rectangle: ";
+  cin >> l >> b;
+
+  int area = l*b;
+  int perimeter = 2*(l+b);
+
+  cout << "Area is = " << area << endl;
+  cout << "Perimeter is = " << perimeter << endl;
+  
   return 0;
 }
