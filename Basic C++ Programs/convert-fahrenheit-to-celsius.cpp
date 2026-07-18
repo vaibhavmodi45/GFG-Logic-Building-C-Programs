5ef75e6865c3738e6be5f931952089180607c46d
Vaibhav Modi <123352253+vaibhavmodi45@users.noreply.github.com>
2026-07-18 23:55:28 +0530

    Add Fahrenheit to Celsius temperature conversion program

--- Basic C++ Programs/convert-fahrenheit-to-celsius.cpp
+++ Basic C++ Programs/convert-fahrenheit-to-celsius.cpp
@@ -1,6 +1,15 @@
-﻿#include <bits/stdc++.h>
+﻿//Fahrenheit to Celsius Tempreture
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  return 0;
+    float temp;
+    cout << "Enter Tempreture in Fehrenheit = ";
+    cin >> temp;
+    
+    float temp_cel = (temp - 32) * (5.0 / 9.0);
+    
+    cout << "Tempreture in Celsius is = " << temp_cel << endl;
+    return 0;
 }
